591fbe7a50b462a93e95f743a8f4a657f7a958dd
TechHara <techhara@outlook.com>
2023-03-07 12:10:29 -0500

    import buffer from python to c++ with string_view

--- cpp_py.cc
+++ cpp_py.cc
@@ -1,16 +1,24 @@
-#include <pybind11/embed.h>
 #include <iostream>
-
+#include <vector>
+#include <string_view>
+#include <pybind11/embed.h>
 namespace py = pybind11;
 
-void exec(std::string const& file) {
-	auto cmd = "with open(\"" + file + "\", \"rb\") as f:\n\tprint(len(f.read()))\n";
-	py::exec(cmd);
-}
-
 int main(int argc, const char** argv) {
-	py::scoped_interpreter guard{}; // start the interpreter and keep it alive
-	std::string file = argc >= 2 ? argv[1] : "/dev/stdin";
-	exec(file);
-	return 0;
+    py::scoped_interpreter guard{};
+
+    std::string input_file = argc >= 2 ? argv[1] : "/dev/stdin";
+
+    // Define a variable in Python
+    py::globals()["input_file"] = input_file;
+    py::exec(R"(
+        with open(input_file, "rb") as f:
+            buf = f.read()
+    )");
+
+    // Access the variable from C++
+    py::object buf = py::globals()["buf"];
+    auto content = buf.cast<std::string_view>();
+    std::cout << content.size() << "\n";
+    return 0;
 }
